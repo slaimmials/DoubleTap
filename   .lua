9a0d8004f57df63f53b549714a827bc0f70a65ef
slaimmials <77630130+slaimmials@users.noreply.github.com>
2024-07-18 18:14:20 +0300

    001

---   .lua
+++   .lua
@@ -23,7 +23,7 @@ function AddFN(Name, FunctionOn, FunctionOff, Color)
         TextLabel.BackgroundTransparency = 1
         TextLabel.TextColor3 = Color
         TextLabel.Text = Name.." >> OFF"
-        TextLabel.Size = UDim2.fromOffset(342,13)
+        TextLabel.Size = UDim2.fromOffset(342,16)
         TextLabel.Position = UDim2.fromOffset(0,#functions.list*TextLabel.Size.Y.Offset)
         TextLabel.TextSize = 14
         TextLabel.TextXAlignment = Enum.TextXAlignment.Left
@@ -47,7 +47,7 @@ function AddButton(Name, Function, Color)
         TextLabel.BackgroundTransparency = 1
         TextLabel.TextColor3 = Color
         TextLabel.Text = Name
-        TextLabel.Size = UDim2.fromOffset(342,13)
+        TextLabel.Size = UDim2.fromOffset(342,16)
         TextLabel.Position = UDim2.fromOffset(0,#functions.list*TextLabel.Size.Y.Offset)
         TextLabel.TextSize = 14
         TextLabel.TextXAlignment = Enum.TextXAlignment.Left
@@ -65,8 +65,8 @@ end
 local hooks = {}
 
 local UIS = game:GetService("UserInputService")
-
-hooks[#hooks+1] = UIS.InputEnded:Connect(function(input)
+local hookC = 0
+hookC=hookC+1;hooks["InputService"..hookC] = UIS.InputEnded:Connect(function(input)
     if input.KeyCode == Enum.KeyCode.Down then
         functions.selected = math.clamp(functions.selected + 1, 1, #functions.list)
     elseif input.KeyCode == Enum.KeyCode.Up then
@@ -89,7 +89,7 @@ hooks[#hooks+1] = UIS.InputEnded:Connect(function(input)
     end
 end)
 
-hooks[#hooks+1] = game:GetService("RunService").RenderStepped:Connect(function()
+hookC=hookC+1;hooks["InterfaceSystem"..hookC] = game:GetService("RunService").RenderStepped:Connect(function()
     if functions.selected ~= 0 then
         local selectedTable = functions.list[functions.selected]
         if selectedTable ~= nil then
@@ -106,7 +106,7 @@ hooks[#hooks+1] = game:GetService("RunService").RenderStepped:Connect(function()
     end
 end)
 
-hooks[#hooks+1] = game:GetService("RunService").Stepped:Connect(function()
+hookC=hookC+1;hooks["InterfaceSystem"..hookC] = game:GetService("RunService").Stepped:Connect(function()
     for _,v in pairs(functions.list) do
         if v.Index ~= functions.selected then
             v.Instance.TextStrokeTransparency = 1
@@ -114,7 +114,9 @@ hooks[#hooks+1] = game:GetService("RunService").Stepped:Connect(function()
     end
 end)
 
-local unloaded = false
+
+
+
 
 function GetDist(dist1, dist2)
     if typeof(dist1) == "Instance" then
@@ -141,13 +143,27 @@ end
 AddFN("Highlight Cups", function()
     local char = game.Players.LocalPlayer.Character
     spawn(function()
-        while wait() do
-            if unloaded == true then
-                break
+        hooks["CupHighlight"] = true
+        local function hookCheck()
+            print"checking"
+            print(hooks["CupHighlight"])
+            if hooks["CupHighlight"] == nil then
+                for _, inst in pairs(workspace:GetChildren()) do
+                    if inst.Name == "a" then
+                        inst:Remove()
+                    end
+                end
+                return false
+            else
+                return true
             end
+        end
+        while wait() do
+            if not hookCheck() then break end
             local closestArena = nil
             local dist = math.huge
             for _i,Arena in pairs(workspace.ArenasREAL:GetChildren()) do
+                if not hookCheck() then break end
                 Arena.Name = "Arena"
                 if (workspace.CurrentCamera.Focus.Position - Arena.CFrame.Position).Magnitude < dist then
                     dist = (workspace.CurrentCamera.Focus.Position - Arena.CFrame.Position).Magnitude
@@ -169,6 +185,7 @@ AddFN("Highlight Cups", function()
                 local searching = true
                 local Dpos = {}
                 repeat
+                    if not hookCheck() then break end
                     for _,diamond in pairs(closestArena.Important.Diamonds:GetChildren()) do
                         if diamond.Transparency == 0 then
                             local mostCup = nil
@@ -179,7 +196,11 @@ AddFN("Highlight Cups", function()
                                     mostCup = Cup
                                 end
                             end
-                            Dpos[#Dpos+1] = mostCup.Name
+                            if #Dpos == 1 and Dpos[#Dpos] ~= mostCup.Name then
+                                Dpos[#Dpos+1] = mostCup.Name
+                            elseif #Dpos == 0 then
+                                Dpos[#Dpos+1] = mostCup.Name
+                            end
                         end
                     end
                     if Dpos[1] ~= nil and Dpos[2] ~= nil then
@@ -202,28 +223,54 @@ AddFN("Highlight Cups", function()
                     hl2.FillTransparency = 1
                     hl2.OutlineColor = Color3.new(1,0,0)
                     repeat
+                        if not hookCheck() then break end
                         hl1.Adornee = closestArena.Important[bypassedName][Dpos[1]]
                         hl2.Adornee = closestArena.Important[bypassedName][Dpos[2]]
                         if not closestArena.Important:FindFirstChild("Diamonds") then
                             IsCups = false
                         end
-                        wait(1)
+                        wait(0.01)
                     until not IsCups
                 end
             end
         end
     end)
-end, function()end)
+end, function()
+    hooks["CupHighlight"] = nil
+end)
+
+local UpdatesGui = Instance.new("TextLabel")
+UpdatesGui.Visible = false
+UpdatesGui.Size = UDim2.fromScale(0.257,0.256)
+UpdatesGui.Position = UDim2.fromScale(0.527,0.744)
+UpdatesGui.BackgroundTransparency = 1
+UpdatesGui.TextColor3 = Color3.new(1,1,1)
+UpdatesGui.TextStrokeTransparency = 0
+UpdatesGui.Text = game:HttpGet("https://raw.githubusercontent.com/slaimmials/DoubleTap/main/addings")
+UpdatesGui.TextXAlignment = Enum.TextXAlignment.Left
+UpdatesGui.TextYAlignment = Enum.TextYAlignment.Top
+UpdatesGui.TextSize = 10
+UpdatesGui.Parent = gui
+AddButton("Updates", function()
+    UpdatesGui.Visible = not UpdatesGui.Visible
+end, Color3.new(0,1,0))
 
 AddButton("Unload", function()
-    unloaded = true
     for _name,hook in pairs(hooks) do
         if typeof(_name) == "string" then
-            print("> unloading '".._name.."'")
+            if typeof(hook) ~= "boolean" then
+                print("> unloading '".._name:sub(1,#_name-1).."'")
+            else
+                print("> unloading '".._name.."'")
+            end
         else
             print"> unloading 'undefined'"
         end
-        hook:Disconnect()
+        if typeof(hook) ~= "boolean" then
+            hook:Disconnect()
+        else
+            hooks[_name] = nil
+        end
     end
     gui:Destroy()
     print"unloaded successfully ☑️"
